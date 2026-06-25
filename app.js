14fd54b1f8bd694644212b50ad1222db5204da23
vladimirosanchez-maker <295971807+vladimirosanchez-maker@users.noreply.github.com>
2026-06-25 18:23:37 -0500

    version solana

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ async function actualizarETH(){
 
         const precio = Number(datos.price);
 
-console.log(datos);
+
         document.getElementById("ethPrecio").innerHTML =
         precio.toLocaleString("es-CO",{
             minimumFractionDigits:2,
@@ -76,3 +76,39 @@ actualizarETH();
 
 setInterval(actualizarETH,3000);
 
+async function actualizarSOL(){
+
+    try{
+
+        const respuesta = await fetch(
+            "https://fapi.binance.com/fapi/v1/ticker/price?symbol=SOLUSDT"
+        );
+
+        const datos = await respuesta.json();
+
+        const precio = Number(datos.price);
+
+        document.getElementById("solPrecio").innerHTML =
+        precio.toLocaleString("es-CO",{
+            minimumFractionDigits:2,
+            maximumFractionDigits:2
+        });
+
+        const ahora = new Date();
+
+        document.getElementById("horaSOL").innerHTML =
+        "Actualizado: " +
+        ahora.toLocaleTimeString("es-CO");
+
+    }catch(error){
+
+        document.getElementById("solPrecio").innerHTML =
+        "Sin conexión";
+
+    }
+
+}
+
+actualizarSOL();
+
+setInterval(actualizarSOL,3000);
