d0209b1512ed487efdefd8983b66a5ab153cb157
vladimirosanchez-maker <295971807+vladimirosanchez-maker@users.noreply.github.com>
2026-06-25 16:22:48 -0500

    V6.2 Agregado ETH

--- app.js
+++ app.js
@@ -38,3 +38,40 @@ actualizarBTC();
 
 setInterval(actualizarBTC,3000);
 
+async function actualizarETH(){
+
+    try{
+
+        const respuesta = await fetch(
+            "https://fapi.binance.com/fapi/v1/ticker/price?symbol=ETHUSDT"
+        );
+
+        const datos = await respuesta.json();
+
+        const precio = Number(datos.price);
+
+        document.getElementById("ethPrecio").innerHTML =
+        precio.toLocaleString("es-CO",{
+            minimumFractionDigits:2,
+            maximumFractionDigits:2
+        });
+
+        const ahora = new Date();
+
+        document.getElementById("horaETH").innerHTML =
+        "Actualizado: " +
+        ahora.toLocaleTimeString("es-CO");
+
+    }catch(error){
+
+        document.getElementById("ethPrecio").innerHTML =
+        "Sin conexión";
+
+    }
+
+}
+
+actualizarETH();
+
+setInterval(actualizarETH,3000);
+
