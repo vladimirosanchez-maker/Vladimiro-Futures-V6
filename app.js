ac67e87bb10b0794666e5589e3c26ff3f9e90a8a
vladimirosanchez-maker <295971807+vladimirosanchez-maker@users.noreply.github.com>
2026-06-25 19:45:32 -0500

    Validacion app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const monedas = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT"
+];
+
+const intervalos = {};
+
 async function actualizarBTC(){
 
     try{
@@ -112,3 +120,62 @@ async function actualizarSOL(){
 actualizarSOL();
 
 setInterval(actualizarSOL,3000);
+
+// NUEVO SISTEMA
+
+async function actualizarMoneda(moneda){
+
+    try{
+
+        const respuesta = await fetch(
+            "https://fapi.binance.com/fapi/v1/ticker/price?symbol=" + moneda
+        );
+
+        const datos = await respuesta.json();
+
+        const precio = Number(datos.price);
+
+        const simbolo = moneda.replace("USDT","").toLowerCase();
+
+        const idPrecio = simbolo + "Precio";
+
+const idHora = "hora" + simbolo.toUpperCase();
+
+const ahora = new Date();        
+
+document.getElementById(idPrecio).innerHTML =
+            precio.toLocaleString("es-CO",{
+                minimumFractionDigits:2,
+                maximumFractionDigits:2
+            });
+            
+ document.getElementById(idHora).innerHTML =
+    "Actualizado: " +
+    ahora.toLocaleTimeString("es-CO");           
+
+    }catch(error){
+
+        console.log("Error:",moneda);
+
+    }
+
+}
+
+
+for (const moneda of monedas){
+
+    actualizarMoneda(moneda);
+
+}
+
+setInterval(function(){
+
+    for (const moneda of monedas){
+
+        actualizarMoneda(moneda);
+
+    }
+
+},3000);
+
+console.log("Nuevo sistema iniciado");
